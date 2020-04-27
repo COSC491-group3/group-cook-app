ad840362eb32814d6c66752682302707a0266f4f
Jada Grant <jagra27@morgan.edu>
2020-04-27 00:06:08 -0400

    Google Map Marker when tapped, goes to menu view controller

--- QuickCooks/HomeViewController.swift
+++ QuickCooks/HomeViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 import GoogleMaps
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, GMSMapViewDelegate {
+    
+    var mapView = GMSMapView()
+    let marker = GMSMarker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,16 +21,24 @@ class HomeViewController: UIViewController {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
         self.view.addSubview(mapView)
 
         // Creates a marker in the center of the map.
-        let marker = GMSMarker()
+        
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         marker.title = "Sydney"
         marker.snippet = "Australia"
         marker.map = mapView
+        mapView.delegate = self
     }
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        print("tapped")
+        self.performSegue(withIdentifier: "toMenuSegue", sender: nil)
+        return true
+    }
+    
 
 
 }
---
+++ QuickCooks/MenuViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MenuViewController.swift
+//  QuickCooks
+//
+//  Created by Jada Grant on 4/26/20.
+//  Copyright © 2020 Jada Grant. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItem")!
+        
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
